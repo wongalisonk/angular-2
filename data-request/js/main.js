90f8652b766390534cdbfcb4e0b44b20c06f4950
Alison <wongalisonk@gmail.com>
2015-11-02 10:12:07 -0800

    First changes

--- data-request/js/main.js
+++ data-request/js/main.js
@@ -1,4 +1,13 @@
 // Create application
+var myApp = angular.module('myApp', []);
+
+sortApp.controller('mainController', function($scope, $http){
+    $scope.sortType = 'name'
+    $http.get('data/salaries.json').then(function(response) {
+        $scope.data = response.data     
+        console.log($scope.data)
+    })
+})
 
 
 // Bind controller customersCtrl
